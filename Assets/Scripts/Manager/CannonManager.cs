365a65f7758ea3be504eaff2c7a4b8773104465c
Pocoloco115 <waltorres@uamv.edu.ni>
2026-05-31 13:17:11 -0600

    agregar: disparo de proyectil + predecir colisiones en la trayectoria

--- Assets/Scripts/Manager/CannonManager.cs
+++ Assets/Scripts/Manager/CannonManager.cs
@@ -2,22 +2,32 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 public class CannonManager : MonoBehaviour
 {
+    private static readonly int PathAmountId = Shader.PropertyToID("_PathAmount");
+
     [SerializeField] private Slider _cannonDegreeSliderValue;
     [SerializeField] private Slider _shotPowerSliderValue;
     [SerializeField] private Transform _shotOrigin;
     [SerializeField] private LineRenderer _lineRenderer;
+    [SerializeField] private GameObject _shotPrefab;
     private int _steps = 250;
     private float _timeInterval = 0.02f;
     private List<Vector3> _shotPathPoints = new List<Vector3>();
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private MaterialPropertyBlock _lineRendererPropertyBlock;
+    private float _shotPathLength;
+    private InputAction _fireAction;
+    private void Awake()
+    {
+        _fireAction = new InputAction("Fire", binding: "<Mouse>/leftButton");
+        _fireAction.performed += ctx => OnFireButtonPressed();
+        _fireAction.Enable();
+    }
     void Start()
     {
-        
+        _lineRendererPropertyBlock = new MaterialPropertyBlock();
     }
-
-    // Update is called once per frame
     void Update()
     {
         UpdateCannonAngle();
@@ -31,6 +41,7 @@ public class CannonManager : MonoBehaviour
     private void CalculatePath()
     {
         _shotPathPoints.Clear();
+        _shotPathLength = 0f;
 
         float shotPower = HandleShotPower();
         float degree = GetCurrentCannonDegree();
@@ -38,13 +49,39 @@ public class CannonManager : MonoBehaviour
         float Vx = Mathf.Cos(degree * Mathf.Deg2Rad) * shotPower;
         float Vy = Mathf.Sin(degree * Mathf.Deg2Rad) * shotPower;
 
+        Vector3 previousPoint = Vector3.zero;
+        bool hasPreviousPoint = false;
+
         for(int i = 0; i < _steps; i++)
         {
             float t = i * _timeInterval;
             float x = Vx * t + _shotOrigin.position.x;
             float y = Vy * t - (0.5f * 9.81f * t * t) + _shotOrigin.position.y;
-            _shotPathPoints.Add(new Vector3(x, y, 0));
+            Vector3 currentPoint = new Vector3(x, y, 0);
+            if(!IsPlaceFree(currentPoint))
+            {
+                break;
+            }
+            _shotPathPoints.Add(currentPoint);
+
+            if(hasPreviousPoint)
+            {
+                _shotPathLength += Vector3.Distance(previousPoint, currentPoint);
+            }
+
+            previousPoint = currentPoint;
+            hasPreviousPoint = true;
         }
+    }
+    private void OnFireButtonPressed()
+    {
+        float shotPower = HandleShotPower();
+        float degree = GetCurrentCannonDegree();
+
+        Vector3 shotDirection = new Vector3(Mathf.Cos(degree * Mathf.Deg2Rad), Mathf.Sin(degree * Mathf.Deg2Rad), 0);
+        GameObject shot = Instantiate(_shotPrefab, _shotOrigin.position, Quaternion.identity);
+        Rigidbody2D shotRigidbody = shot.GetComponent<Rigidbody2D>();
+        shotRigidbody.linearVelocity = shotDirection * shotPower;
     }
     private float HandleShotPower()
     {
@@ -54,6 +91,11 @@ public class CannonManager : MonoBehaviour
     {
         return transform.rotation.eulerAngles.z;
     }
+    private bool IsPlaceFree(Vector3 position)
+    {
+        Collider2D hitCollider = Physics2D.OverlapCircle(position, 0.1f);
+        return hitCollider == null;
+    }
     private void RenderPath()
     {
         if(_lineRenderer == null)
@@ -62,5 +104,14 @@ public class CannonManager : MonoBehaviour
         }
         _lineRenderer.positionCount = _shotPathPoints.Count;
         _lineRenderer.SetPositions(_shotPathPoints.ToArray());
+
+        if(_lineRendererPropertyBlock == null)
+        {
+            _lineRendererPropertyBlock = new MaterialPropertyBlock();
+        }
+
+        _lineRenderer.GetPropertyBlock(_lineRendererPropertyBlock);
+        _lineRendererPropertyBlock.SetFloat(PathAmountId, Mathf.Max(1f, _shotPathLength));
+        _lineRenderer.SetPropertyBlock(_lineRendererPropertyBlock);
     }
 }
